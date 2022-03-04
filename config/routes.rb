fcbf5fe7f0178a60a220f054ffafaca9e7d138af
Justin Luciano <justinlluciano@gmail.com>
2022-03-04 09:56:08 -0800

    commit tree 2829040889b37441102b268570669faf034d3967

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  post '/signup', to: 'users#signup'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
